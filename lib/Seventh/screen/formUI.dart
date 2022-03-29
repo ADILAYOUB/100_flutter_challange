6037e8aab50c31f6fcd11849d20cb359344669c9
AdilAyoub <mintofree840@gmail.com>
2022-03-29 16:17:23 +0530

    Login Screen

---
+++ lib/Seventh/screen/formUI.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Widget _get_FormUI(){
+  return Column(
+    children:const  <Widget>[
+      Icon(Icons.person,
+      color: Colors.lightBlue,
+      size: 100.0,
+      ),
+      SizedBox(height: 50.0),
+    ],
+  );
+}
