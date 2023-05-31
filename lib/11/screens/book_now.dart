c0534080eccf1882ddd5d0857a1c830ab78fa500
ADILAYOUB <mintofree840@gmail.com>
2023-05-31 21:18:12 +0530

    added book_now

--- lib/11/screens/book_now.dart
+++ lib/11/screens/book_now.dart
@@ -1,10 +1,72 @@
+import 'package:flutte_challange/11/constants/string/image_url.dart';
 import 'package:flutter/material.dart';
 
-class BookNow extends StatelessWidget {
-  const BookNow({super.key});
+class BookingPage extends StatelessWidget {
+  const BookingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Container(
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(36),
+            color: Colors.grey.shade800,
+          ),
+          child: Center(
+            child: IconButton(
+              icon: const Icon(
+                Icons.arrow_back_ios_new,
+                size: 16,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        ),
+        title: const Center(
+          child: Text(
+            'Book Now',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 100,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                    width: 80,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade600,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                            backgroundImage: NetworkImage(
+                          ImageUrl().profilePic,
+                        ))
+                      ],
+                    )),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
