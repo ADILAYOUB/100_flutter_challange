cabdaa4c0157d4c1911d7f898c6824498dc76f04
ADILAYOUB <mintofree840@gmail.com>
2023-06-18 21:02:46 +0530

    quiz app app

---
+++ lib/questions.dart
@@ -0,0 +1,167 @@
+import 'questions_model.dart';
+
+List<QuestionsModel> questions = [
+  QuestionsModel(
+    question: 'Which year was the Indian Constitution adopted?',
+    answer: '1950',
+    options: ['1947', '1949', '1950', '1952'],
+  ),
+  QuestionsModel(
+    question: 'Who is known as the Father of the Indian Constitution?',
+    answer: 'B.R. Ambedkar',
+    options: [
+      'Jawaharlal Nehru',
+      'Mahatma Gandhi',
+      'B.R. Ambedkar',
+      'Sardar Vallabhbhai Patel'
+    ],
+  ),
+  // QuestionsModel(
+  //   question: 'How many articles are there in the Indian Constitution?',
+  //   answer: '448',
+  //   options: ['365', '448', '520', '617'],
+  // ),
+  // QuestionsModel(
+  //   question:
+  //       'Which article of the Indian Constitution abolishes untouchability?',
+  //   answer: 'Article 17',
+  //   options: ['Article 15', 'Article 16', 'Article 17', 'Article 18'],
+  // ),
+  // QuestionsModel(
+  //   question: 'Who is the current President of India?',
+  //   answer: 'Ram Nath Kovind',
+  //   options: ['Narendra Modi', 'Rahul Gandhi', 'Amit Shah', 'Ram Nath Kovind'],
+  // ),
+  // QuestionsModel(
+  //   question:
+  //       'What is the minimum age required to become the President of India?',
+  //   answer: '35 years',
+  //   options: ['30 years', '35 years', '40 years', '45 years'],
+  // ),
+  // QuestionsModel(
+  //   question: 'Who appoints the Chief Justice of India?',
+  //   answer: 'President of India',
+  //   options: [
+  //     'Prime Minister of India',
+  //     'President of India',
+  //     'Supreme Court Judges',
+  //     'Attorney General of India'
+  //   ],
+  // ),
+  // QuestionsModel(
+  //   question: 'Which amendment introduced the Anti-Defection Law in India?',
+  //   answer: '52nd Amendment',
+  //   options: [
+  //     '42nd Amendment',
+  //     '44th Amendment',
+  //     '52nd Amendment',
+  //     '56th Amendment'
+  //   ],
+  // ),
+  // QuestionsModel(
+  //   question:
+  //       'What is the total number of schedules in the Indian Constitution?',
+  //   answer: '12',
+  //   options: ['8', '10', '12', '14'],
+  // ),
+  // QuestionsModel(
+  //   question:
+  //       'Which fundamental right is guaranteed by Article 19 of the Indian Constitution?',
+  //   answer: 'Right to Freedom',
+  //   options: [
+  //     'Right to Equality',
+  //     'Right to Freedom',
+  //     'Right against Exploitation',
+  //     'Right to Constitutional Remedies'
+  //   ],
+  // ),
+  // QuestionsModel(
+  //   question: 'Who has the power to declare a financial emergency in India?',
+  //   answer: 'President of India',
+  //   options: [
+  //     'Prime Minister of India',
+  //     'President of India',
+  //     'Finance Minister',
+  //     'Comptroller and Auditor General'
+  //   ],
+  // ),
+  // QuestionsModel(
+  //   question:
+  //       'Which article of the Indian Constitution deals with the Right to Education?',
+  //   answer: 'Article 21A',
+  //   options: ['Article 19A', 'Article 21A', 'Article 22A', 'Article 24A'],
+  // ),
+  // QuestionsModel(
+  //   question: 'What is the maximum number of members in the Rajya Sabha?',
+  //   answer: '250',
+  //   options: ['200', '225', '250', '275'],
+  // ),
+  // QuestionsModel(
+  //   question: 'Who is the head of the Union Executive in India?',
+  //   answer: 'President',
+  //   options: [
+  //     'Prime Minister',
+  //     'President',
+  //     'Chief Justice',
+  //     'Speaker of Lok Sabha'
+  //   ],
+  // ),
+  // QuestionsModel(
+  //   question: 'Who acts as the Chairman of the Rajya Sabha?',
+  //   answer: 'Vice President',
+  //   options: [
+  //     'President',
+  //     'Prime Minister',
+  //     'Vice President',
+  //     'Speaker of Lok Sabha'
+  //   ],
+  // ),
+  // QuestionsModel(
+  //   question: 'Which amendment lowered the voting age from 21 to 18 years?',
+  //   answer: '61st Amendment',
+  //   options: [
+  //     '42nd Amendment',
+  //     '50th Amendment',
+  //     '61st Amendment',
+  //     '74th Amendment'
+  //   ],
+  // ),
+  // QuestionsModel(
+  //   question:
+  //       'Which article of the Indian Constitution deals with the Right to Information?',
+  //   answer: 'Article 19',
+  //   options: ['Article 17', 'Article 19', 'Article 21', 'Article 24'],
+  // ),
+  // QuestionsModel(
+  //   question: 'Who has the power to appoint the Governor of a state in India?',
+  //   answer: 'President of India',
+  //   options: [
+  //     'Prime Minister of India',
+  //     'Governor of the concerned state',
+  //     'Chief Minister of the concerned state',
+  //     'President of India'
+  //   ],
+  // ),
+  // QuestionsModel(
+  //   question: 'Which amendment introduced the Panchayati Raj system in India?',
+  //   answer: '73rd Amendment',
+  //   options: [
+  //     '42nd Amendment',
+  //     '50th Amendment',
+  //     '61st Amendment',
+  //     '73rd Amendment'
+  //   ],
+  // ),
+  // QuestionsModel(
+  //   question:
+  //       'What is the term of office of a Judge of the Supreme Court of India?',
+  //   answer: 'Until the age of 65 years',
+  //   options: [
+  //     '10 years',
+  //     'Until retirement',
+  //     'Until the age of 65 years',
+  //     '5 years'
+  //   ],
+  // ),
+  // // Add more questions here...
+];
---
+++ lib/questions_model.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class QuestionsModel {
+  QuestionsModel({
+    required this.question,
+    required this.answer,
+    required this.options,
+  });
+  final String question;
+  final String answer;
+  final List<String> options;
+}
+
+//List of questions 
