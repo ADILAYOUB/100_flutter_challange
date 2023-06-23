037c8cc22ae6d3407a8d1997c261d19afe2133dc
ADILAYOUB <mintofree840@gmail.com>
2023-06-23 00:00:00 -0600

    added employee department and profile

--- lib/Employee Attendence/services/database_service.dart
+++ lib/Employee Attendence/services/database_service.dart
@@ -2,14 +2,19 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutterchallenge/Employee%20Attendence/models/user_model.dart';
+import 'package:flutterchallenge/Employee%20Attendence/utils/snakebar.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../constants/constants.dart';
+import '../models/department_model.dart';
 
 class DatabaseService extends ChangeNotifier {
   // get instance
   final SupabaseClient _supabase = Supabase.instance.client;
   UserModel? userModel;
+  //list of departments
+  List<DepartmentModel> alldepartments = [];
+  int? employeeDepartment;
   // for employee id we generate a random id
   String generateRandomEmployeeId() {
     final random = Random();
@@ -40,6 +45,27 @@ class DatabaseService extends ChangeNotifier {
         .single();
 
     userModel = UserModel.fromJson(userData);
+    employeeDepartment == null
+        ? employeeDepartment = userModel!.department
+        : null;
     return userModel!;
+  }
+
+  // user can update the department and name in the profile
+
+  Future<void> getAllDepartments() async {
+    final List result =
+        await _supabase.from(Constants.departmentTable).select();
+    alldepartments = result.map((e) => DepartmentModel.fromJson(e)).toList();
+    notifyListeners();
+  }
+
+  Future updateProfile(String name, BuildContext context) async {
+    await _supabase.from(Constants.employeeTable).update({
+      'name': name,
+      'department': employeeDepartment,
+    }).eq('id', _supabase.auth.currentUser!.id);
+    Common.showSnackBar('Profile Updated', context, color: Colors.green);
+    notifyListeners();
   }
 }
