1ee57780974dd8a0cf27defb34a096382f990c55
ADILAYOUB <mintofree840@gmail.com>
2023-07-15 18:00:50 +0530

    updated home screen

--- lib/Movie Application/view/home_screen.dart
+++ lib/Movie Application/view/home_screen.dart
@@ -71,80 +71,8 @@ class HomeScreen extends StatelessWidget {
                         fontWeight: FontWeight.w400,
                         color: Colors.grey.shade400),
                   ),
-                )
+                ),
               ]),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            ListView.builder(
-              itemCount: 12,
-              padding: const EdgeInsets.all(16),
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.3,
-                  width: double.infinity,
-                  child: Container(
-                    width: MediaQuery.of(context).size.height * 0.43,
-                    margin: const EdgeInsets.only(right: 15),
-                    clipBehavior: Clip.antiAlias,
-                    decoration: BoxDecoration(
-                      color: Colors.grey.withOpacity(0.1),
-                      borderRadius: const BorderRadius.all(Radius.circular(30)),
-                      image: const DecorationImage(
-                          image: NetworkImage(
-                              'https://th.bing.com/th/id/OIP.YTvPkPqknzMBQmTiojSr_gHaFB?pid=ImgDet&rs=1'),
-                          fit: BoxFit.fill),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.1),
-                          blurRadius: 1,
-                          offset: const Offset(0, 2),
-                        ),
-                      ],
-                    ),
-                    child: Container(
-                      padding: const EdgeInsets.all(15),
-                      decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(5))),
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Flexible(
-                              flex: 3,
-                              child: Text(
-                                'Move name',
-                                style: theme.textTheme.titleMedium!.copyWith(
-                                  fontWeight: FontWeight.w800,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                const Icon(
-                                  IconlyLight.star,
-                                  size: 18,
-                                  color: Colors.yellow,
-                                ),
-                                const SizedBox(
-                                  width: 5,
-                                ),
-                                Text(
-                                  '****',
-                                  style: theme.textTheme.titleSmall!
-                                      .copyWith(fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            )
-                          ]),
-                    ),
-                  ),
-                );
-              },
             ),
           ],
         ),
