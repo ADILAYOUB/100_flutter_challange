ad0887d83cb73ed035f753b03d0c24d14aed225b
ADILAYOUB <mintofree840@gmail.com>
2023-09-15 10:53:34 +0530

    tic tac toe

--- lib/Employee Attendence/constants/constants.dart
+++
@@ -1,5 +0,0 @@
-class Constants {
-  static const employeeTable = 'employees';
-  static const departmentTable = 'departments';
-  static const attendanceTable = 'attendance';
-}
--- lib/Movie Application/utils/constants.dart
+++
@@ -1,1 +0,0 @@
-const String appBarTitle = 'Movie Latest';
